a7d797e22abd28e5b5d955164be07d21cf2a0b0a
edumel20 <rabdanmelianeduardo@gmail.com>
2024-02-26 23:37:59 +0000

    Update tarea_13.sql

---
+++ Unidad-5/Tareas/tarea_13.sql
@@ -0,0 +1,292 @@
+/**
+Consultas de una única tabla
+**/
+
+--Devuelve un listado con el primer apellido, segundo apellido y el nombre de todos los alumnos. El listado deberá estar ordenado alfabéticamente de menor a mayor por el primer apellido, segundo apellido y nombre.
+select apellido1, apellido2, nombre from persona ORDER BY apellido1, apellido2, nombre asc;
+
+/**
++------------+------------+-----------+
+| apellido1  | apellido2  |  nombre   |
++------------+------------+-----------+
+| Domínguez  | Guerrero   | Antonio   |
+| Fahey      | Considine  | Antonio   |
+| Gea        | Ruiz       | Sonia     |
+| Gutiérrez  | López      | Juan      |
+| Hamill     | Kozey      | Manolo    |
+| Heller     | Pagac      | Pedro     |
+| Herman     | Pacocha    | Daniel    |
+| Hernández  | Martínez   | Irene     |
+| Herzog     | Tremblay   | Ramón     |
+| Kohler     | Schoen     | Alejandro |
+| Koss       | Bayer      | José      |
+| Lakin      | Yundt      | Inma      |
+| Lemke      | Rutherford | Cristina  |
+| Monahan    | Murray     | Micaela   |
+| Ramirez    | Gea        | Zoe       |
+| Ruecker    | Upton      | Guillermo |
+| Saez       | Vega       | Juan      |
+| Schmidt    | Fisher     | David     |
+| Schowalter | Muller     | Francesca |
+| Spencer    | Lakin      | Esther    |
+| Stiedemann | Morissette | Alfredo   |
+| Streich    | Hirthe     | Carmen    |
+| Strosin    | Turcotte   | Ismael    |
+| Sánchez    | Pérez      | Salvador  |
++------------+------------+-----------+
+**/
+
+--Averigua el nombre y los dos apellidos de los alumnos que no han dado de alta su número de teléfono en la base de datos.
+select nombre, apellido1, apellido2 from persona WHERE telefono IS NULL;
+
+/**
++-----------+------------+-----------+
+|  nombre   | apellido1  | apellido2 |
++-----------+------------+-----------+
+| Pedro     | Heller     | Pagac     |
+| Ismael    | Strosin    | Turcotte  |
+| Esther    | Spencer    | Lakin     |
+| Carmen    | Streich    | Hirthe    |
+| Antonio   | Fahey      | Considine |
+| Guillermo | Ruecker    | Upton     |
+| Francesca | Schowalter | Muller    |
++-----------+------------+-----------+
+**/
+
+--Devuelve el listado de los alumnos que nacieron en 1999.
+--select * from persona WHERE strftime('%Y', fecha) = '1999';
+select * from persona WHERE fecha_nacimiento regexp '1999';
+/**
++----+-----------+---------+-----------+-----------+---------+-----------------+-----------+------------------+------+--------+
+| id |    nif    | nombre  | apellido1 | apellido2 | ciudad  |    direccion    | telefono  | fecha_nacimiento | sexo |  tipo  |
++----+-----------+---------+-----------+-----------+---------+-----------------+-----------+------------------+------+--------+
+| 7  | 97258166K | Ismael  | Strosin   | Turcotte  | Almería | C/ Neptuno      |           | 1999/05/24       | H    | alumno |
+| 22 | 41491230N | Antonio | Domínguez | Guerrero  | Almería | C/ Cabo de Gata | 626652498 | 1999/02/11       | H    | alumno |
++----+-----------+---------+-----------+-----------+---------+-----------------+-----------+------------------+------+--------+
+**/
+
+--Devuelve el listado de profesores que no han dado de alta su número de teléfono en la base de datos y además su nif termina en K.
+ select * from persona WHERE tipo = 'profesor' and telefono IS NULL and nif regexp 'K$';
+
+/**
++----+-----------+-----------+-----------+-----------+---------+---------------------------+----------+------------------+------+----------+
+| id |    nif    |  nombre   | apellido1 | apellido2 | ciudad  |         direccion         | telefono | fecha_nacimiento | sexo |   tipo   |
++----+-----------+-----------+-----------+-----------+---------+---------------------------+----------+------------------+------+----------+
+| 16 | 10485008K | Antonio   | Fahey     | Considine | Almería | C/ Sierra de los Filabres |          | 1982/03/18       | H    | profesor |
+| 17 | 85869555K | Guillermo | Ruecker   | Upton     | Almería | C/ Sierra de Gádor        |          | 1973/05/05       | H    | profesor |
++----+-----------+-----------+-----------+-----------+---------+---------------------------+----------+------------------+------+----------+
+**/
+
+--Devuelve el listado de las asignaturas que se imparten en el primer cuatrimestre, en el tercer curso del grado que tiene el identificador 7.
+select id, nombre from asignatura WHERE cuatrimestre = 1 and curso = 3 and id_grado = 7 ORDER BY id;
+
+/**
++----+-------------------------------------------+
+| id |                  nombre                   |
++----+-------------------------------------------+
+| 72 | Bases moleculares del desarrollo vegetal  |
+| 73 | Fisiología animal                         |
+| 74 | Metabolismo y biosíntesis de biomoléculas |
+| 75 | Operaciones de separación                 |
+| 76 | Patología molecular de plantas            |
+| 77 | Técnicas instrumentales básicas           |
++----+-------------------------------------------+
+**/
+
+/**
+Consultas multitabla (Join)
+**/
+
+--Devuelve un listado con los datos de todas las alumnas que se han matriculado alguna vez en el Grado en Ingeniería Informática (Plan 2015).
+select * from persona pe,  
+--Devuelve un listado con todas las asignaturas ofertadas en el Grado en Ingeniería Informática (Plan 2015).
+select asig.id, asig.nombre, g.id from asignatura asig JOIN grado g ON asig.id_grado = g.id WHERE g.nombre = 'Grado en Ingeniería Informática (Plan 2015)' ORDER BY asig.id;
+
+/**
++----+--------------------------------------------------------------+----+
+| id |                            nombre                            | id |
++----+--------------------------------------------------------------+----+
+| 1  | Álgegra lineal y matemática discreta                         | 4  |
++----+--------------------------------------------------------------+----+
+| 2  | Cálculo                                                      | 4  |
++----+--------------------------------------------------------------+----+
+| 3  | Física para informática                                      | 4  |
++----+--------------------------------------------------------------+----+
+| 4  | Introducción a la programación                               | 4  |
++----+--------------------------------------------------------------+----+
+| 5  | Organización y gestión de empresas                           | 4  |
++----+--------------------------------------------------------------+----+
+| 6  | Estadística                                                  | 4  |
++----+--------------------------------------------------------------+----+
+| 7  | Estructura y tecnología de computadores                      | 4  |
++----+--------------------------------------------------------------+----+
+| 8  | Fundamentos de electrónica                                   | 4  |
++----+--------------------------------------------------------------+----+
+| 9  | Lógica y algorítmica                                         | 4  |
++----+--------------------------------------------------------------+----+
+| 10 | Metodología de la programación                               | 4  |
++----+--------------------------------------------------------------+----+
+| 11 | Arquitectura de Computadores                                 | 4  |
++----+--------------------------------------------------------------+----+
+| 12 | Estructura de Datos y Algoritmos I                           | 4  |
++----+--------------------------------------------------------------+----+
+| 13 | Ingeniería del Software                                      | 4  |
++----+--------------------------------------------------------------+----+
+| 14 | Sistemas Inteligentes                                        | 4  |
++----+--------------------------------------------------------------+----+
+| 15 | Sistemas Operativos                                          | 4  |
++----+--------------------------------------------------------------+----+
+| 16 | Bases de Datos                                               | 4  |
++----+--------------------------------------------------------------+----+
+| 17 | Estructura de Datos y Algoritmos II                          | 4  |
++----+--------------------------------------------------------------+----+
+| 18 | Fundamentos de Redes de Computadores                         | 4  |
++----+--------------------------------------------------------------+----+
+| 19 | Planificación y Gestión de Proyectos Informáticos            | 4  |
++----+--------------------------------------------------------------+----+
+| 20 | Programación de Servicios Software                           | 4  |
++----+--------------------------------------------------------------+----+
+| 21 | Desarrollo de interfaces de usuario                          | 4  |
++----+--------------------------------------------------------------+----+
+| 22 | Ingeniería de Requisitos                                     | 4  |
++----+--------------------------------------------------------------+----+
+| 23 | Integración de las Tecnologías de la Información en las Orga | 4  |
+|    | nizaciones                                                   |    |
++----+--------------------------------------------------------------+----+
+| 24 | Modelado y Diseño del Software 1                             | 4  |
++----+--------------------------------------------------------------+----+
+| 25 | Multiprocesadores                                            | 4  |
++----+--------------------------------------------------------------+----+
+| 26 | Seguridad y cumplimiento normativo                           | 4  |
++----+--------------------------------------------------------------+----+
+| 27 | Sistema de Información para las Organizaciones               | 4  |
++----+--------------------------------------------------------------+----+
+| 28 | Tecnologías web                                              | 4  |
++----+--------------------------------------------------------------+----+
+| 29 | Teoría de códigos y criptografía                             | 4  |
++----+--------------------------------------------------------------+----+
+| 30 | Administración de bases de datos                             | 4  |
++----+--------------------------------------------------------------+----+
+| 31 | Herramientas y Métodos de Ingeniería del Software            | 4  |
++----+--------------------------------------------------------------+----+
+| 32 | Informática industrial y robótica                            | 4  |
++----+--------------------------------------------------------------+----+
+| 33 | Ingeniería de Sistemas de Información                        | 4  |
++----+--------------------------------------------------------------+----+
+| 34 | Modelado y Diseño del Software 2                             | 4  |
++----+--------------------------------------------------------------+----+
+| 35 | Negocio Electrónico                                          | 4  |
++----+--------------------------------------------------------------+----+
+| 36 | Periféricos e interfaces                                     | 4  |
++----+--------------------------------------------------------------+----+
+| 37 | Sistemas de tiempo real                                      | 4  |
++----+--------------------------------------------------------------+----+
+| 38 | Tecnologías de acceso a red                                  | 4  |
++----+--------------------------------------------------------------+----+
+| 39 | Tratamiento digital de imágenes                              | 4  |
++----+--------------------------------------------------------------+----+
+| 40 | Administración de redes y sistemas operativos                | 4  |
++----+--------------------------------------------------------------+----+
+| 41 | Almacenes de Datos                                           | 4  |
++----+--------------------------------------------------------------+----+
+| 42 | Fiabilidad y Gestión de Riesgos                              | 4  |
++----+--------------------------------------------------------------+----+
+| 43 | Líneas de Productos Software                                 | 4  |
++----+--------------------------------------------------------------+----+
+| 44 | Procesos de Ingeniería del Software 1                        | 4  |
++----+--------------------------------------------------------------+----+
+| 45 | Tecnologías multimedia                                       | 4  |
++----+--------------------------------------------------------------+----+
+| 46 | Análisis y planificación de las TI                           | 4  |
++----+--------------------------------------------------------------+----+
+| 47 | Desarrollo Rápido de Aplicaciones                            | 4  |
++----+--------------------------------------------------------------+----+
+| 48 | Gestión de la Calidad y de la Innovación Tecnológica         | 4  |
++----+--------------------------------------------------------------+----+
+| 49 | Inteligencia del Negocio                                     | 4  |
++----+--------------------------------------------------------------+----+
+| 50 | Procesos de Ingeniería del Software 2                        | 4  |
++----+--------------------------------------------------------------+----+
+| 51 | Seguridad Informática                                        | 4  |
++----+--------------------------------------------------------------+----+
+**/
+
+--Devuelve un listado de los profesores junto con el nombre del departamento al que están vinculados. El listado debe devolver cuatro columnas, primer apellido, segundo apellido, nombre y nombre del departamento. El resultado estará ordenado alfabéticamente de menor a mayor por los apellidos y el nombre.
+
+--Devuelve un listado con el nombre de las asignaturas, año de inicio y año de fin del curso escolar del alumno con nif 26902806M.
+
+--Devuelve un listado con el nombre de todos los departamentos que tienen profesores que imparten alguna asignatura en el Grado en Ingeniería Informática (Plan 2015).
+
+--Devuelve un listado con todos los alumnos que se han matriculado en alguna asignatura durante el curso escolar 2018/2019.
+
+/**
+Consultas multitabla (Where)
+**/
+
+--Devuelve un listado con los nombres de todos los profesores y los departamentos que tienen vinculados. El listado también debe mostrar aquellos profesores que no tienen ningún departamento asociado. El listado debe devolver cuatro columnas, nombre del departamento, primer apellido, segundo apellido y nombre del profesor. El resultado estará ordenado alfabéticamente de menor a mayor por el nombre del departamento, apellidos y el nombre.
+
+--Devuelve un listado con los profesores que no están asociados a un departamento.
+
+--Devuelve un listado con los departamentos que no tienen profesores asociados.
+
+--Devuelve un listado con los profesores que no imparten ninguna asignatura.
+
+--Devuelve un listado con las asignaturas que no tienen un profesor asignado.
+
+--Devuelve un listado con todos los departamentos que tienen alguna asignatura que no se haya impartido en ningún curso escolar. El resultado debe mostrar el nombre del departamento y el nombre de la asignatura que no se haya impartido nunca.
+
+/**
+Consultas resúmen (Funciones)
+**/
+
+--Devuelve el número total de alumnas que hay.
+select COUNT(id) AS numero_alumnas from persona WHERE sexo = 'M' and tipo = 'alumno';
+
+/**
++----------------+
+| numero_alumnas |
++----------------+
+| 3              |
++----------------+
+**/
+
+--Calcula cuántos alumnos nacieron en 1999.
+select COUNT(id) AS nacidos_1999 from persona WHERE fecha_nacimiento regexp '1999' and tipo = 'alumno';
+
+/**
++--------------+
+| nacidos_1999 |
++--------------+
+| 2            |
++--------------+
+**/
+
+--Calcula cuántos profesores hay en cada departamento. El resultado sólo debe mostrar dos columnas, una con el nombre del departamento y otra con el número de profesores que hay en ese departamento. El resultado sólo debe incluir los departamentos que tienen profesores asociados y deberá estar ordenado de mayor a menor por el número de profesores.
+
+--Devuelve un listado con todos los departamentos y el número de profesores que hay en cada uno de ellos. Tenga en cuenta que pueden existir departamentos que no tienen profesores asociados. Estos departamentos también tienen que aparecer en el listado.
+
+--Devuelve un listado con el nombre de todos los grados existentes en la base de datos y el número de asignaturas que tiene cada uno. Tenga en cuenta que pueden existir grados que no tienen asignaturas asociadas. Estos grados también tienen que aparecer en el listado. El resultado deberá estar ordenado de mayor a menor por el número de asignaturas.
+
+--Devuelve un listado con el nombre de todos los grados existentes en la base de datos y el número de asignaturas que tiene cada uno, de los grados que tengan más de 40 asignaturas asociadas.
+
+--Devuelve un listado que muestre el nombre de los grados y la suma del número total de créditos que hay para cada tipo de asignatura. El resultado debe tener tres columnas: nombre del grado, tipo de asignatura y la suma de los créditos de todas las asignaturas que hay de ese tipo. Ordene el resultado de mayor a menor por el número total de crédidos.
+
+--Devuelve un listado que muestre cuántos alumnos se han matriculado de alguna asignatura en cada uno de los cursos escolares. El resultado deberá mostrar dos columnas, una columna con el año de inicio del curso escolar y otra con el número de alumnos matriculados.
+
+--Devuelve un listado con el número de asignaturas que imparte cada profesor. El listado debe tener en cuenta aquellos profesores que no imparten ninguna asignatura. El resultado mostrará cinco columnas: id, nombre, primer apellido, segundo apellido y número de asignaturas. El resultado estará ordenado de mayor a menor por el número de asignaturas.
+
+/**
+Subconsultas
+**/
+
+--Devuelve todos los datos del alumno más joven.
+
+--Devuelve un listado con los profesores que no están asociados a un departamento.
+
+--Devuelve un listado con los departamentos que no tienen profesores asociados.
+
+--Devuelve un listado con los profesores que tienen un departamento asociado y que no imparten ninguna asignatura.
+
+--Devuelve un listado con las asignaturas que no tienen un profesor asignado.
+
+--Devuelve un listado con todos los departamentos que no han impartido asignaturas en ningún curso escolar.
