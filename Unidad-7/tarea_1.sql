cefa67b787a412c62cb7749d849912c6caa9c84a
edumel20 <rabdanmelianeduardo@gmail.com>
2024-04-03 11:35:36 +0100

    se hace tarea_1.sql de la Unidad-7

--- Unidad-7/tarea_1.sql
+++ Unidad-7/tarea_1.sql
@@ -1,1 +1,25 @@
+/**
+TAREA 1 DE TRABAJO CON ÍNDICES
+**/
+
+-- 1. Elimine la tabla "alumno si existe":
+drop table if exists alumno;
+
+-- 2. Cree la tabla definiendo una clave primaria compuesta (año de inscripción y número de inscripción):
+CREATE DATABASE IF NOT EXISTS tarea_1;
+
+USE tarea_1
+
+CREATE TABLE alumnos (
+    numero_inscripcion INT AUTO_INCREMENT,
+    fecha_inscripcion INT,
+    nombre VARCHAR(50),
+    dni VARCHAR(50),
+    domicilio VARCHAR(50),
+    ciudad VARCHAR(50),
+    provincia VARCHAR(50),
+    PRIMARY KEY (numero_inscripcion, fecha_inscripcion) 
+);
+
+describe alumnos;
 
